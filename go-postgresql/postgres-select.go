76ef4330b77b436eaa06d7931032b2724ca96670
dongguangming <dongguangming.love@gmail.com>
2020-07-17 17:53:52 +0800

    golang crud postgresql

---
+++ go-postgresql/postgres-select.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+        //_ "github.com/bmizerany/pq"
+        
+)
+
+const (
+	// TODO fill this in directly or through environment variable
+	// Build a DSN e.g. postgres://username:password@url.com:5432/dbName
+	DB_DSN = "postgres://postgres:12345678@192.168.8.200:5432/douyin?sslmode=disable"
+)
+
+type User struct {
+	ID       int
+	Email    string
+	Password string
+}
+
+func main() {
+
+	// Create DB pool
+	//db, err := sql.Open("postgres", "host=192.168.8.200 port=5432 user=postgres password=12345678 dbname=douyin sslmode=disable")
+	db, err := sql.Open("postgres",DB_DSN)
+        if err != nil {
+		log.Fatal("Failed to open a DB connection: ", err)
+	}
+	defer db.Close()
+
+	// Create an empty user and make the sql query (using $1 for the parameter)
+	var myUser User
+	userSql := "SELECT id, email, password FROM users WHERE id = $1"
+
+	err = db.QueryRow(userSql, 1).Scan(&myUser.ID, &myUser.Email, &myUser.Password)
+	if err != nil {
+		log.Fatal("Failed to execute query: ", err)
+	}
+
+	fmt.Printf("hello email: %s, password: %s, welcome back!\n", myUser.Email,myUser.Password)
+
+}
---
+++ go-postgresql/postgres-update.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+        //_ "github.com/bmizerany/pq"
+        
+)
+
+const (
+	// TODO fill this in directly or through environment variable
+	// Build a DSN e.g. postgres://username:password@url.com:5432/dbName
+	DB_DSN = "postgres://postgres:12345678@192.168.8.200:5432/douyin?sslmode=disable"
+)
+
+type User struct {
+	ID       int
+	Email    string
+	Password string
+}
+
+func main() {
+
+	// Create DB pool
+	//db, err := sql.Open("postgres", "host=192.168.8.200 port=5432 user=postgres password=12345678 dbname=douyin sslmode=disable")
+	db, err := sql.Open("postgres",DB_DSN)
+        if err != nil {
+		log.Fatal("Failed to open a DB connection: ", err)
+	}
+	defer db.Close()
+
+
+        var user User = User{ID:4,Email:"dong@qq.com",Password:"abcdedf120"}
+         _, err = db.Exec("UPDATE  users SET email=$1, password=$2 where id=$3", user.Email,user.Password,user.ID)
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Printf("update ok!!!")
+
+	// Create an empty user and make the sql query (using $1 for the parameter)
+	var myUser User
+	userSql := "SELECT id, email, password FROM users WHERE id = $1"
+
+	err = db.QueryRow(userSql, 4).Scan(&myUser.ID, &myUser.Email, &myUser.Password)
+	if err != nil {
+		log.Fatal("Failed to execute query: ", err)
+	}
+
+	fmt.Printf("hello email: %s, password: %s, welcome back!\n", myUser.Email,myUser.Password)
+
+}
